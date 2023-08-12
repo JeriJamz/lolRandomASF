5fd219d02aa901eeec3baf9d3e9a1e739ef65e89
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-08-12 16:12:18 -0500

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,27 @@
+import time,sys, pyautogui as pag
+#this is the logic
+
+def sprint(z): #the slow print
+    for y in z:
+        sys.stdout.write(y)
+        sys.stdout.flush()
+        time.sleep(.05)
+
+#make a list that will hold 1 - 64
+def TodoLists():#idk kno if this is vaild yet
+    newItem = input()
+    tDlist = list(range(str(int(x,64))))
+    for x in range(tDlist(newItem)):
+        x = newItem
+    goto nput
+        
+            
+
+
+
+
+sprint('return 0')
+
+nput = input(newItem)
+
+
