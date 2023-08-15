10f8839e00e3da59dbb3ad0a0829d2a8377e46ab
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-08-14 21:54:01 -0500

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 import time,sys, pyautogui as pag
 #this is the logic
+#this is now an app
 global nput
 
 def sprint(z): #the slow print
@@ -9,20 +10,42 @@ def sprint(z): #the slow print
         time.sleep(.05)
 
 #make a list that will hold 1 - 64
-def TodoList():#idk kno if this is vaild yet
-    newItem = input()
-    nput = input()
-    tDlist = list(range(str(int(x,64))))#this gone give me hell
-    for x in range(tDlist(newItem)):
-        x + 1
+
+class ToDo:
+    def __init__(self, TodoList):
+        self.TodoList
+        self.click
+        self.clickNdrag
+        self.UiFunc
+        self.Commands
+
+    def TodoList():#idk kno if this is vaild yet
+        newItem = input()#working on a print and idk if this takes the strings or just the numbers yet
+        nput = input()#dont think i need this
+        tDlist = list(range(str(int(x,64))))#this gone give me hell
+        for x in range(tDlist(newItem)):#this is the algo to store the item for the day. Max is 64 for the day
+            x + 1
+
+    def Click():#from here down are "features ima add"
+        pass
+
+    def clickNdrag():
+        pass
+
+    def UiFunc():
+        pass
+
+    def Commands():
+        pass
     
 sprint('return 0\n')
 tr = True
-"Exit" == False
+
 nput = input()
 
-if nput.lower() == "yes":
+while nput.lower() == "yes":
     while tr:
+        f'<Exit>' == sys.exit  
         print("Todo Listing")
         nput = input()
         print(f'{nput}')
