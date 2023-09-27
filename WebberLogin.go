b96e60a026b35b1861a0c1239dedaebf72d1b46b
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-09-27 10:03:24 -0500

    Add files via upload

---
+++ WebberLogin.go
@@ -0,0 +1,17 @@
+import("fmt"
+       "os"
+        )
+
+package main
+
+func main(){
+
+	goto login
+
+}
+
+func login(){
+
+	fmt.Println("Welcome to the site\nEnter your UserName and Login")
+
+}
--- WebberServe.go
+++ WebberServe.go
@@ -54,3 +54,4 @@ func checkError(err error){
     }
 
 }
+
