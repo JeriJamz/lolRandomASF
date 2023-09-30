27d5e6e88b8eece84090331809aa6e9d19c2d00e
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-09-30 10:16:25 -0500

    Add files via upload

--- WebberLogin.go
+++ WebberLogin.go
@@ -4,6 +4,45 @@ import("fmt"
 
 package main
 
+/**********************Exsiting User********************/
+
+type Usr struct{
+
+    UsrName UserName
+    PassWrd PassWord
+    Email []Email
+}
+
+type UsrName struct{
+
+    UserName string
+    Email []Email
+
+}
+
+type PassWrd struct{
+
+    Password string
+
+}
+
+type Email stuct{
+
+    Address string
+
+}
+
+/**********************New User**************************/
+
+type NewUser struct{
+
+    Email Email[]
+    usrName UserName
+    Passwrd PassWord
+
+}
+
+
 func main(){
 
 	goto login
